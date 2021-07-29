e45c72f291d33ee1ae9775a7211a3786d2814bdd
manInit <info.denisbronnikov@gmail.com>
2021-07-29 22:33:43 +0900

    refactor and fix bug

--- src/utils/workWithArray.js
+++ src/utils/workWithArray.js
@@ -1,18 +1,17 @@
 const arrMove = (step, arr) => {
-  const movedArr = [];
+  let movedArr;
+  step = step % arr.length;
+
   if (step < 0) {
-    step = -step % arr.length;
-    for (let i = step; i < arr.length; i++) 
-      movedArr.push(arr[i]); 
-    for (let i = 0; i < step; i++)
-      movedArr.push(arr[i]);
+    const firstPart = arr.slice(-arr.length - step);
+    const secondPart = arr.slice(0, -arr.length - step);
+    movedArr = firstPart.concat(secondPart);
   } else {
-    step = step % arr.length;
-    for (let i = arr.length - 1; i > arr.length - 1 - step; i--) 
-      movedArr.push(arr[i]); 
-    for (let i = 0; i <= arr.length - 1 - step; i++)
-      movedArr.push(arr[i]);
+    const firstPart = arr.slice(0, arr.length - step);
+    const secondPart = arr.slice(arr.length - step);
+    movedArr = secondPart.concat(firstPart);
   }
+
   return movedArr;
 };
 
