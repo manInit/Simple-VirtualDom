7ac7a3474d833679b7756c1600acb3dee71de913
manInit <info.denisbronnikov@gmail.com>
2022-04-17 14:37:53 +0800

    update entry point

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,15 @@
 import './globalStyles/reset.css'
 
-import { Slider } from './components/imageSlider/index'
-import { BasicComponent, createElement, mount } from './library/vdom/index'
-import { VirtualElem } from './library/vdom/VirtualElem.js'
+import { TestComponent } from './components/test/index'
+import { MainComponent } from './components/main/index'
+import { CounterComponent } from './components/counter/index'
 
-class TestComponent implements BasicComponent {
-  private urlImages
-  private slider
-  public state = {}
+import { Router } from './library/router/index'
 
-  constructor() {
-    this.urlImages = [
-      'https://webstatic-sea.mihoyo.com/upload/event/2021/06/08/457a8eb51ff9a08294e4cd89d53f8ea8_9165375142265154823.jpg',
-      'https://webstatic-sea.mihoyo.com/upload/event/2021/06/07/84ff40efc1d42323bda6a4887a2f2fd9_8179715898099820308.jpg',
-      'https://webstatic-sea.mihoyo.com/upload/event/2021/06/07/1c0a4f33927fc1356fc6fdeef1b693b9_6307666152615946317.jpg',
-      'https://webstatic-sea.mihoyo.com/upload/event/2021/06/07/f92a5c7a6087d27df987f570281fb713_1620003236594776086.jpg',
-      'https://webstatic-sea.mihoyo.com/upload/event/2021/06/07/ed8f7007d0f94238d8e9818b64ab52fa_7052252726274558929.jpg'
-    ]
-    this.slider = new Slider(this.urlImages)
-  }
+Router.setRoutes({
+  '/': new MainComponent(),
+  '/slider': new TestComponent(),
+  '/counter': new CounterComponent()
+})
 
-  getVEl(): VirtualElem {
-    return createElement('section', {
-      children: [ this.slider ],
-      attrs: {}
-    })
-  }
-}
-
-mount(new TestComponent(), document.getElementById('root'))
+// mount(new MainComponent(), document.getElementById('root'))
